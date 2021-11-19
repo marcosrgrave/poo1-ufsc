a9c50dd63ad31fb217acd57acc7f153e8828bc52
Marcos PC <marcos.r.grave@gmail.com>
2021-11-18 21:41:12 -0300

    faltam 4

--- 4.revisao_prova1.py
+++ 4.revisao_prova1.py
@@ -1,3 +1,6 @@
+from time import process_time, time
+
+
 def quadrant():
     respostas = []
     while True:
@@ -20,7 +23,14 @@ def quadrant():
 
 
 def what_is_the_fastest():
-    pass
+    times = list(map(float, input().split()))  # time in seconds
+    competitors = ['Otavio', 'Bruno', 'Ian']
+    podium = sorted(times)  # winner has the lowest time
+    if podium[0] == podium[1]:
+        print('Empate')
+    else:
+        print(competitors[times.index(podium[0])])
+
 
 
 def capuchin_monkey():
@@ -28,19 +38,67 @@ def capuchin_monkey():
 
 
 def bafo():
-    pass
+    resps = list()
+    c = 1
+    while True:
+        rodadas = int(input())
+        if rodadas == 0:
+            break
+        
+        sumA = sumB = 0
+        for i in range(0, rodadas):
+            pts = list(map(int, input().strip().split()))
+            sumA += pts[0]
+            sumB += pts[1]
+        winner = 'Aldo' if sumA > sumB else 'Beto'
+        resps.append(f'Teste {c}\n{winner}\n')
+        c += 1
+    
+    for r in resps:
+        print(r)
 
 
 def easy_diff_btwn_dates():
-    pass
+    from datetime import date
+    entryA = input().strip().split()
+    entryB = input().strip().split()
+    dateA = date(2021, int(entryA[1]), int(entryA[0]))
+    dateB = date(2021, int(entryB[1]), int(entryB[0]))
+    dif = dateB - dateA
+    print(dif.days)
 
 
 def elevator():
-    pass
+    elevator_data = list(map(int, input().strip().split()))
+    sensor_readings, max_capacity = elevator_data[0], elevator_data[1]
+    
+    exccess = False
+    elevator_occupancy = 0
+    for i in range(0, sensor_readings):
+        movement = list(map(int, input().strip().split()))
+        came_out, came_in = movement[0], movement[1]
+        elevator_occupancy += came_in - came_out
+        if elevator_occupancy > max_capacity:
+            exccess = True
+
+    print('S') if exccess else print('N')
 
 
 def escada_rolante():
-    pass
+    number_people = int(input())
+    delay = 10  # seconds
+
+    person_data = list()
+    for i in range(0, number_people):
+        person_data.append(int(input()))
+        if i == 0:
+            total_time = 0
+        elif (dif:= person_data[i] - person_data[i-1]) <= delay:
+            total_time += dif
+            
+        if i == number_people-1:
+            total_time += delay
+    print(total_time)
 
 
 def album_fotos():
@@ -48,7 +106,11 @@ def album_fotos():
 
 
 def chocolate():
-    pass
+    L = int(input())  # lado em cm
+    if L >= 2:
+        print(L**2) if L % 2 == 0 else print((L-1)**2)
+    else:
+        print(1)
 
 
 def saldo_do_vovo():
@@ -63,4 +125,4 @@ def kikoho():
     pass
 
 
-quadrant()
+chocolate()
