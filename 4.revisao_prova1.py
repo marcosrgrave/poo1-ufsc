8e4c369f2a604a756ec042454c3789b51865c640
Marcos PC <marcos.r.grave@gmail.com>
2021-11-21 20:16:01 -0300

    9/12

--- 4.revisao_prova1.py
+++ 4.revisao_prova1.py
@@ -30,10 +30,47 @@ def what_is_the_fastest():
         print(competitors[times.index(podium[0])])
 
 
-
 def capuchin_monkey():
-    pass
+    resps = []
+    i = 1
+    while True:
+        qtd_retangulos = int(input())
+        if qtd_retangulos == 0:
+            break
+        
+        # Pegando os pontos dos retangulos
+        listx1 = list()
+        listx2 = list()
+        listy1 = list()
+        listy2 = list()
+        for retangulo in range(0, qtd_retangulos):
+            entry = input().strip().split()
+            X1, Y1, X2, Y2 = int(entry[0]), int(entry[1]), int(entry[2]), int(entry[3])
 
+            # pts_temp = []
+            # for n in range(X1, X2+1):
+            #     pts_temp.append(n)
+            # ptsX.append(pts_temp[:])
+            
+            # pts_temp.clear()
+            # for n in range(Y2, Y1+1):
+            #     pts_temp.append(n)
+            # ptsY.append(pts_temp[:])
+
+            listx1.append(X1)
+            listx2.append(X2)
+            listy1.append(Y1)
+            listy2.append(Y2)
+
+        # Filtrando os pontos em comum
+        comum_x = [max(listx1), min(listx2)]  # esse tipo de filtro não indica se existem pontos em comum
+        comum_y = [min(listy1), max(listy2)]
+
+        resps.append(f'Teste {i}\n{comum_x[0]} {comum_y[0]} {comum_x[1]} {comum_y[1]}\n')
+        i += 1
+
+    for r in resps:
+        print(r)
 
 def bafo():
     resps = list()
@@ -204,4 +241,4 @@ def kikoho():
 
         print(f'{area_resp:.3f}')
 
-kikoho()
+capuchin_monkey()
