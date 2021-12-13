72135c70eaf3e54572087f100bf632750ecf77e5
win11 <marcos.r.grave@gmail.com>
2021-12-13 20:49:55 -0300

    3/18

--- prova2_listas/6.strings.py
+++ prova2_listas/6.strings.py
@@ -51,20 +51,36 @@ def led():  # OK
         print(s, 'leds')
 
 
-def wertyu():
+def wertyu():  # INCOMPLETO
+    # tá faltando um print na ultima linha de todas (udebug)
     while True:
         try:
-            querty = "1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./"
+            querty = {'1':"`", '2':'1', '3':'2', '4':'3', '5':'4','6':'5',
+                '7':'6', '8':'7', '9':'8', '0':'9', '-':'0','=':'-',
+                'W':'Q','E':'W','R':'E','T':'R','Y':'T','U':'Y', 'I':'U',
+                'O':'I','P':'O','S':'A','D':'S','F':'D','G':'F','H':'G','J':'H',
+                'K':'J','L':'K','X':'Z','C':'X','V':'C','B':'V','N':'B','M':'N',
+                ',':'M','.':',','/':'.', ' ':' ', '[':'P',']':'[','\\':']',
+                ';':'L',"'":';', 'A':'', 'Q':'', 'Z':'', '`':''}
             txt = input().strip()
             for char in txt:
-                if char == ' ':
-                    print(' ', end='')
-                else:
-                    i = querty.index(char.upper())
-                    print(querty[i-1], end='')
+                print(' ', end='') if char == ' ' else print(querty[char], end='')
             print()
         except EOFError:
             break
+
+
+            # querty = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./"
+            # txt = input().strip()
+            # for char in txt:
+            #     if char == ' ':
+            #         print(' ', end='')
+            #     else:
+            #         i = querty.index(char.upper())
+            #         print(querty[i-1], end='')
+            # print()
+        # except EOFError:
+        #     break
 
 
 def combiner():
