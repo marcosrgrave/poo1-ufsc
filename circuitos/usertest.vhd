7ca7c6fb6471148477a5a42f6081f14b3797babf
note_m1 <marcos.r.grave@gmail.com>
2021-12-15 09:15:19 -0300

    added vhdl files

--- circuitos/usertest.vhd
+++ circuitos/usertest.vhd
@@ -22,7 +22,7 @@ begin
         B => SB,
         Sum => SSum,
         Cout => SCout
-    )
+    );
     SA <= '0', '1' after 20 ns, '0' after 40 ns;
     --  SA recebe 0 inicialmente e depois de 20 nano segundos, recebe 1, e etc.
     -- esse 'after'só serve para testbench, nao para projeto.
